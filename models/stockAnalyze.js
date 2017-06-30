165744544a851b95f633d0491f6dd6a130b1f1ae
Buchanan, Anthony (CORP) <Anthony.Buchanan@adp.com>
2017-06-30 09:15:17 -0400

    updated model

--- models/stockAnalyze.js
+++ models/stockAnalyze.js
@@ -6,7 +6,8 @@ const Schema = mongoose.Schema;
 let StockAnalyze = new Schema({
 	ticker : String,
 	last : String,
-	percentage : String
+	percentage : String,
+	dateAdded : {type: Date, default: Date.now}
 })
 
 module.exports = StockAnalyze = mongoose.model('StockAnalyze', StockAnalyze);

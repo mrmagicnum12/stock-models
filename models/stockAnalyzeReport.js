fa90b67b0e2877e9e7ce056a2753d2efc5da79b5
Buchanan, Anthony (CORP) <Anthony.Buchanan@adp.com>
2017-07-01 19:08:15 -0400

    added new model

--- models/stockAnalyzeReport.js
+++ models/stockAnalyzeReport.js
@@ -3,11 +3,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+let Report = new Schema({
+	ticker : String,
+	low : Number,
+	high : Number,
+	average : Number
+})
+
 let StockAnalyzeReport = new Schema({
 	analyticType : String,
 	type: String,
 	hour: Number,
 	day: String,
+	report: [Report],
 	dateRecorded: {type:Date, default: Date.now}
 })
 
